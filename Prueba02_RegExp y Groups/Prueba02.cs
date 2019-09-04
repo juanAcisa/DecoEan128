da0899c7b5427091000672c68ca65cbf4b6260e6
juan sanchez <juan.acisa@gmail.com>
2019-09-04 23:40:37 +0200

    Agregar archivos de proyecto.

---
+++ Prueba02_RegExp y Groups/Prueba02.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace EAN128
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\d{3})-(\d{3}-\d{4})";
+            string input = "212-555-6666 906-932-1111 415-222-3333 425-888-9999";
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine("Grupo 1: \t\t{0}", match.Groups[1].Value);
+                Console.WriteLine("Grupp 2: \t\t{0}", match.Groups[2].Value);
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+
+    //Output:
+    //
+    //Area Code:        212
+    //Telephone number: 555-6666
+    //
+    //Area Code:        906
+    //Telephone number: 932-1111
+    //
+    //Area Code:        415
+    //Telephone number: 222-3333
+    //
+    //Area Code:        425
+    //Telephone number: 888-9999
+
---
+++ Pruebas/CodeFile1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class CodeFile1
+{
+    public static void Main()
+    {
+        string input = "This is one sentence. This is another.";
+        string pattern = @"\b(\w+[;,]?\s?)+[.?!]";
+        //string input = "Esta es la primera sentencia. Esta es la otra sentencia. Y esto no tiene nada que ver con las sentencias anteriores.";
+        //string pattern = @"([eE]st[aeiou])\ .*\.";
+
+        Console.WriteLine("Input is: {0}", input);
+
+        foreach (Match match in Regex.Matches(input, pattern))
+        {
+            Console.WriteLine("Match: '{0}' at index {1}.",
+                              match.Value, match.Index);
+            int grpCtr = 0;
+            foreach (Group grp in match.Groups)
+            {
+                Console.WriteLine("   Group {0}: '{1}' at index {2}.",
+                                  grpCtr, grp.Value, grp.Index);
+                int capCtr = 0;
+                foreach (Capture cap in grp.Captures)
+                {
+                    Console.WriteLine("      Capture {0}: '{1}' at {2}.",
+                                      capCtr, cap.Value, cap.Index);
+                    capCtr++;
+                }
+                grpCtr++;
+            }
+            Console.WriteLine();
+
+        }
+        Console.ReadKey();
+    }
+}
